f1cad308335fed3583ca184acf7e8e52efb59b38
Hari Vemulapalli <hvemulapalli04@gmail.com>
2025-06-09 18:20:58 -0400

    Update interview_logic.py
    Need to fix this for the api key to work on the deployed server.

--- interview_logic.py
+++ interview_logic.py
@@ -3,7 +3,11 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+get_api_key = st.secrets.get("GEMINI_API_KEY", os.getenv("GEMINI_API_KEY"))
+if not api_key:
+    raise ValueError("❌ Gemini API key not found in st.secrets or environment variables.")
+genai.configure(api_key=get_api_key)
+
 
 model = genai.GenerativeModel("gemini-2.0-flash")
 
